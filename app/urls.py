81e1f2f4a5af32cec480e1d8d21936db9e1e728b
AminCht <achytsazan@gmail.com>
2022-10-24 03:51:31 +0330

    first commit

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', views.login_view, name="login"),
+    path('register/', views.register_user, name="register"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path('dashboard/', views.dashboard)
+]
