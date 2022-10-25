686e37dd39f3cdd7df949cb2af0c34375b38f4e0
AminCht <achytsazan@gmail.com>
2022-10-25 20:56:28 +0330

    add 404 for incorrect urls

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.contrib.auth.views import LogoutView, logout_then_login
 
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('register/', views.register_user, name="register"),
     path("logout/", views.logout, name="logout"),
-    path('', views.dashboard)
+    path('', views.dashboard),
+    re_path(r'^.*\.*', views.pages, name='pages'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,9 +4,14 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 # Create your views here.
+from django import template
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.template import loader
+from django.urls import reverse
+
 from .forms import LoginForm, SignUpForm
 from django.contrib.auth.views import LogoutView
 
@@ -74,3 +79,28 @@ def logout(request):
 def dashboard(request):
 
     return render(request, 'home/index.html')
+
+
+def pages(request):
+    context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+
+        load_template = request.path.split('/')[-1]
+
+        if load_template == 'admin':
+            return HttpResponseRedirect(reverse('admin:index'))
+        context['segment'] = load_template
+
+        html_template = loader.get_template('home/' + load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template('home/page-404.html')
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template('home/page-500.html')
+        return HttpResponse(html_template.render(context, request))
