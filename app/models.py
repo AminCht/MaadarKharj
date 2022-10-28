6298cedd2e94d23b1521bf74899f4abf43441299
foad <foad1381@gmail.com>
2022-10-28 15:43:29 +0330

    models

--- app/models.py
+++ app/models.py
@@ -6,12 +6,12 @@ class Customer(models.Model):
     name = models.CharField(max_length=255)
 
 
-class DebtAmount(models.Model):
-    price = models.DecimalField(decimal_places=5, max_digits=10)
-    date = models.DateTimeField(auto_now_add=True)
-    debtor = models.ManyToManyField(Customer, related_name='debtor')
-
-
 class Debt(models.Model):
     creditor = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='creditor')
-    debt_amount = models.ForeignKey(DebtAmount)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class DebtAmount(models.Model):
+    price = models.DecimalField(decimal_places=5, max_digits=10)
+    debtor = models.ManyToManyField(Customer, related_name='debtor')
+    debt = models.ForeignKey(Debt, on_delete=models.CASCADE)
